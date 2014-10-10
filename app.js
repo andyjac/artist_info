8697e03bed8a091d72b6180df01a79f9ccf06ff8
Andrew <andyjacuw@gmail.com>
2014-10-10 13:50:08 -0700

    add seperate function for view render, elminiate trailing commas in albumslist, reorganize order of html

--- app.js
+++ app.js
@@ -1,53 +1,58 @@
 var express = require('express');
 var request = require('request');
 
-// For importing my lastfm API Key
+// import my lastfm API Key
 var lastfmCreds = require('./lastfm_credentials');
 
-// Instantiate the express app
+// instantiate the express app
 var app = express();
 
 app.set('views', './views');
 app.set('view engine', 'jade');
 
 // GET the home page
-app.get('/', function (req, res) {
-  makeRequest(searchOnLastfm, function (artist, topAlbums) {
-    res.render('index', { title: 'Top Albums', artist : 'Top albums for ' + artist + ':', albums : topAlbums});
-  });
+app.get('/', function(req, res) {
+  makeRequest(searchOnLastfm, renderResults, res);
 });
 
 // QUERY is hard-coded for the time being. Eventually will be retrieved from user input
 var QUERY = 'flying+lotus';
 
-// URL of the lastfm API which will return the top albums of a given artist
+// API url which will return the top albums of a given artist
 var searchOnLastfm = 'http://ws.audioscrobbler.com/2.0/?' +
     'method=artist.gettopalbums&' +
     'artist=' + QUERY + '&' +
-    'limit=5&' +
+    'limit=10&' +
     'api_key=' + lastfmCreds + '&' +
     'format=json';
 
-// Request to the API
-function makeRequest(searchOnLastfm, callBack) {
+// request the API
+function makeRequest(urlToSearch, callBack, res) {
   var parsedResponse
     , artist
     , albums
     , album
-    , topAlbums = '';
+    , topAlbums = [];
 
-  request(searchOnLastfm, function(error, response, body) {
+  request(urlToSearch, function(error, response, body) {
     if(!error && response.statusCode === 200) {
       parsedResponse = JSON.parse(body);
       artist = parsedResponse.topalbums.album[0].artist.name;
       albums = parsedResponse.topalbums.album;
       for (var p in albums) {
         album = albums[p].name;
-        topAlbums += album + ', ' ;
+        topAlbums.push(album);
+
       }
+      topAlbums = topAlbums.join(', ');
     }
-    callBack(artist, topAlbums);
+    callBack(artist, topAlbums, res);
   });
+}
+
+// render the retrieved info from the API in the browser
+function renderResults(artist, topAlbums, res) {
+  res.render('index', { title: 'Top Albums', artist : 'Top albums for ' + artist + ':', albums : topAlbums});
 }
 
 app.listen(3000);
