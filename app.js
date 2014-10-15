08e606be2feb4156056ed925f21e942a2c3b0f8b
Andrew <andyjacuw@gmail.com>
2014-10-15 13:57:08 -0700

    remove unnecessary comments to reduce clutter

--- app.js
+++ app.js
@@ -2,11 +2,8 @@ var express = require('express');
 var request = require('request');
 var _ = require('lodash');
 var bodyParser = require('body-parser');
-
-// import my lastfm API Key
 var LASTFM_CREDS = require('./lastfm_credentials');
 
-// instantiate the express app
 var app = express();
 
 app.set('views', './views');
@@ -14,24 +11,20 @@ app.set('view engine', 'jade');
 // set app to use bodyParser to parse a url encoded body response from a request
 app.use(bodyParser.urlencoded({extended: false}));
 
-// POST artist search input
 app.post('/search', function(req, res) {
   var artist = formatArtistInput(req, artist);
   var queryURL = buildTopAlbumsQueryURL(artist);
   requestLastfmTopAlbums(queryURL, _.partial(renderResults, res));
 });
 
-// GET the home page
 app.get('/', function(req, res) {
   res.render('index');
 });
 
-// format artist input and replace ' ' with '+'
 function formatArtistInput(req, artist) {
   return req.body.artist.replace(/\s+/g, '+');
 }
 
-// API url which will return the top albums of a given artist
 function buildTopAlbumsQueryURL(artist) {
   return 'http://ws.audioscrobbler.com/2.0/?' +
     'method=artist.gettopalbums&' +
@@ -41,7 +34,6 @@ function buildTopAlbumsQueryURL(artist) {
     'format=json';
 }
 
-// ensure OK response
 function responseOK(error, response) {
   return !error && response.statusCode === 200;
 }
@@ -59,7 +51,6 @@ function extractTopAlbumInfo(body) {
   };
 }
 
-// request the top 10 albums of an artist on the lastfm API
 function requestLastfmTopAlbums(urlToSearch, callBack) {
   var parsedResponse
     , topAlbumInfo;
@@ -76,8 +67,6 @@ function requestLastfmTopAlbums(urlToSearch, callBack) {
   });
 }
 
-// if there is an error render the error page
-// if no error render top albums in the results page
 function renderResults(res, topAlbumInfo) {
   if(topAlbumInfo.errorCode) {
     res.render('error', topAlbumInfo);
