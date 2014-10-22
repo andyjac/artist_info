b738f6dea0fcbee60198f2d3cfe3f8a1be89c3f8
Andrew <andyjacuw@gmail.com>
2014-10-22 15:10:56 -0700

    respond to request with raw json on the page

--- source/render_results.js
+++ source/render_results.js
@@ -1,8 +1,8 @@
 module.exports = function renderResults(res, topAlbumInfo, errorResponse) {
   if(errorResponse) {
-    res.render('error', errorResponse);
+    res.json(errorResponse);
   }
   else {
-    res.render('results', topAlbumInfo);
+    res.json(topAlbumInfo);
   }
 };
