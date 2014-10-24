1cfea71b3323d089466ed27e2188dc0a3e1b2224
Andrew <andyjacuw@gmail.com>
2014-10-24 11:53:10 -0700

    add client side Jquery functionality

--- source/app.js
+++ source/app.js
@@ -9,6 +9,7 @@ app.set('views', './views');
 app.set('view engine', 'jade');
 // set app to use the bodyParser module to parse a url encoded body response from a request
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static('public'));
 
 app.get('/search', postSearchHandler);
 app.get('/', getRootHandler);
