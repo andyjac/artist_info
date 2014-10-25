1f0ca94b3bbf1b7d72565a1d6742166c8064c975
Andrew <andyjacuw@gmail.com>
2014-10-25 12:27:18 -0700

    add client side jquery to log the results of a highlighted artist query to the console

--- public/javascripts/client_side.js
+++ public/javascripts/client_side.js
@@ -1,6 +1,26 @@
 $(document).ready(function() {
-  $('*').mouseup(function() {
-    var text = window.getSelection().toString();
-    console.log(text);
-  });
+  makeAjaxRequest();
 });
+
+function makeAjaxRequest() {
+  $('body').mouseup(function() {
+    var artist = window.getSelection().toString();
+    $.ajax({
+      url: '/search',
+      type: 'GET',
+      data: {artist: artist},
+      dataType: 'json',
+      success: function(json) {
+        console.log(json);
+      },
+      error: function(xhr, status, errorThrown) {
+        console.log('there was a problem!');
+        console.log('status: ' + status);
+        console.log('error thrown: ' + errorThrown);
+      },
+      complete: function(xhr, status) {
+        console.log('the request was completed');
+      }
+    });
+  });
+}
--- source/render_results.js
+++ source/render_results.js
@@ -1,8 +1,12 @@
 module.exports = function renderResults(res, topAlbumInfo, errorResponse) {
   if(errorResponse) {
+    res.header('Content-type', 'application/json');
+    res.header('Charset', 'utf8');
     res.json(errorResponse);
   }
   else {
+    res.header('Content-type', 'application/json');
+    res.header('Charset', 'utf8');
     res.json(topAlbumInfo);
   }
 };
