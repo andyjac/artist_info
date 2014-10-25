1e5908073adcba6e761252a9ebca45203c7be5e2
bion <bionjohnson@gmail.com>
2014-10-25 01:13:29 -0700

    Rename file to match its exported module name

--- source/post_search_handler.js
+++ source/post_search_handler.js
@@ -1,11 +1,12 @@
 var _ = require('lodash');
 var formatArtistInput = require('./format_artist_input');
 var buildTopAlbumsQueryUrl = require('./build_url');
-var requestLastfmTopAlbums = require('./make_request');
+var requestLastfmTopAlbums = require('./request_lastfm_top_albums');
 var renderResults = require('./render_results');
 
 module.exports = function postSearchHandler(req, res) {
   var artist = formatArtistInput(req);
   var queryUrl = buildTopAlbumsQueryUrl(artist);
+
   requestLastfmTopAlbums(queryUrl, _.partial(renderResults, res));
 };
