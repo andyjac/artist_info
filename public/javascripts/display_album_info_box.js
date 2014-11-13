a77e54bb5c175447989cb4d384b963ab7a1cb9e2
Andrew <andyjacuw@gmail.com>
2014-11-13 12:47:06 -0800

    Rename 'client_side.js' more semantically

---
+++ public/javascripts/display_album_info_box.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+  $('html').mouseup(getHighlightedText);
+});
+var topAlbumsHTML;
+
+function getHighlightedText() {
+  var artist = window.getSelection().toString();
+
+  if (topAlbumsHTML != null) {
+    topAlbumsHTML.remove();
+    delete topAlbumsHTML;
+  }
+  if (artist === '') return;
+  getTopAlbums(artist);
+}
+
+function getTopAlbums(artist) {
+  $.ajax({
+    url: '/search',
+    type: 'GET',
+    data: {artist: artist},
+    dataType: 'json',
+    success: handleTopAlbums,
+    error: function(xhr, status, errorThrown) {
+      console.log('there was a problem!');
+      console.log('status: ' + status);
+      console.log('error thrown: ' + errorThrown);
+    }
+  });
+}
+
+function handleTopAlbums(json) {
+  topAlbumsHTML = $(template(json));
+  topAlbumsHTML.css('display', 'none');
+  $(topAlbumsHTML).appendTo('html').fadeIn(200);
+}
