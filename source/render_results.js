2c896c67d16b1c074895c9a515fc87726c80cfc9
Andrew <andyjacuw@gmail.com>
2014-11-18 12:53:33 -0800

    Add space between 'if' and '(' for consistency

--- source/render_results.js
+++ source/render_results.js
@@ -1,5 +1,5 @@
 module.exports = function renderResults(res, topAlbumInfo, errorResponse) {
-  if (errorResponse) {
+  if(errorResponse) {
     res.header('Content-type', 'application/json');
     res.header('Charset', 'utf8');
     res.json(errorResponse);
