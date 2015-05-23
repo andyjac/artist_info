7b19cef0953d44dc68b01369dfc37bc291cf8078
Andrew Jacobson <andyjacuw@gmail.com>
2015-05-22 23:17:57 -0700

    Rename function to match method

--- source/get_search_handler.js
+++ source/get_search_handler.js
@@ -4,7 +4,7 @@ var buildTopAlbumsQueryUrl = require('./build_top_albums_query_url');
 var requestLastfmTopAlbums = require('./request_lastfm_top_albums');
 var renderResults = require('./render_results');
 
-module.exports = function postSearchHandler(req, res) {
+module.exports = function getSearchHandler(req, res) {
   console.info(['\nDate[', Date.now(), '] Alert[Request From Address: ', req.ip, ']'].join(''));
   var artist = formatArtistInput(req);
   var queryUrl = buildTopAlbumsQueryUrl(artist);
